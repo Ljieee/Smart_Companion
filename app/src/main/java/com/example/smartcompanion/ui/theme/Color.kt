ffd1beba149b5b3becd6cde65bb2ef4b0562d54e
Ljieee <cativoljie501@gmail.com>
2026-02-01 07:25:27 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/smartcompanion/ui/theme/Color.kt
@@ -0,0 +1,7 @@
+package com.example.smartcompanion.ui.theme
+
+import androidx.compose.ui.graphics.Color
+val MidnightBlue = Color(0xFF131A26)
+val CardBackground = Color(0xFF1E2737)
+val GoldAccent = Color(0xFFC9A66B)
+val TextGray = Color(0xFF9DA3AE)
---
+++ app/src/main/java/com/example/smartcompanion/ui/theme/Screens/DashBoardScreen.kt
@@ -0,0 +1,4 @@
+package com.example.smartcompanion.ui.theme.Screens
+
+class DashBoard_Screen {
+}
